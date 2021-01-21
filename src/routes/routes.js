ae5c5e00e8b74fbbf47438ca475f98eaa1015705
Rui Monteiro <rui@ruimonteiro.com>
2021-01-21 18:16:34 +0000

    Completed module

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
-        
+        .get(getProducts)
+
         // Post endpoint
         .post(addnewProduct);
+
+    app.route('/products/:ProductID')
+        // get a specific Product
+        .get(getProductWithID)
+
 }
 
 export default routes;
